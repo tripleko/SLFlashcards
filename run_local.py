3c959ec959fabd3912b3cd8a1133c769dc748194
Jay <tripleko@users.noreply.github.com>
2015-12-30 19:40:56 -0800

    Add port arg parser.

--- run_local.py
+++ run_local.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from app import create_app, db
 from app.models import Interval_pref, Flashcard, Deck
+import argparse
 
 #My application structure was inspired by Miguel Grinberg's book on Flask.
 
@@ -8,5 +9,11 @@ if __name__ == '__main__':
     app = create_app('localserver')
     with app.app_context():
         db.create_all()
-    app.run()
+    
+    parser = argparse.ArgumentParser(description='Run flashcard app')
+    #custom port
+    parser.add_argument('-p', type=int, default=5000)
+    args = parser.parse_args()
+    
+    app.run(port=args.p)
 
